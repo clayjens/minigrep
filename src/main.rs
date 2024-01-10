ace0c8258f7fd4e6daa33f3c7e4586a1373b4f34
Clayton Jensen <clayton@yourmommas.house>
2024-01-09 22:25:31 -0600

    remove debug, oops

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use clap::Parser;
 fn main() {
     let config = minigrep::MinigrepCli::parse();
 
-    dbg!(&config);
-
     if let Err(e) = minigrep::run(&config) {
         eprintln!("application error: {e}");
         std::process::exit(1);
