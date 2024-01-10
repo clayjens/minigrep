f29d66ff4270f585a8eaf625331137a05502e583
Clayton Jensen <clayton@yourmommas.house>
2024-01-09 22:01:51 -0600

    buffered file read

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@
 //! A simple grep-like utility written in Rust, following the tutorial in [The Rust Programming Language](https://doc.rust-lang.org/book/ch12-00-an-io-project.html).
 #![warn(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
 use clap::Parser;
 
@@ -11,28 +16,31 @@ use clap::Parser;
 /// Clap command-line interface for the minigrep application.
 pub struct MinigrepCli {
     #[arg(short, long)]
-    verbose: bool,
+    /// Enable verbose output.
+    pub verbose: bool,
     #[arg(short, long)]
-    ignore_case: bool,
-    query: String,
-    file: PathBuf,
+    /// Perform a case-insensitive search, default false.
+    pub ignore_case: bool,
+    /// The query string to search for.
+    pub query: String,
+    /// The path to file to search in.
+    pub file: PathBuf,
 }
 
 /// Run the minigrep application with the given configuration.
 pub fn run(config: &MinigrepCli) -> Result<(), Box<dyn std::error::Error>> {
-    let contents = std::fs::read_to_string(&config.file)?;
+    let file = File::open(&config.file)?;
+    let file_reader = BufReader::new(file);
 
     let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+        search_case_insensitive_lazy(&config.query, file_reader)
     } else {
-        search(&config.query, &contents)
-    };
+        search_lazy(&config.query, file_reader)
+    }?;
 
     if config.verbose {
         println!("Searching for {}", config.query);
         println!("In file {}", config.file.display());
-        println!("With text:\n{}", contents);
-        println!("Results:");
     }
 
     display(&results);
@@ -41,7 +49,7 @@ pub fn run(config: &MinigrepCli) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 /// Display the given results of a search.
-fn display<'a>(results: &HashMap<usize, &'a str>) {
+fn display<'a>(results: &HashMap<usize, String>) {
     let mut results = results.iter().collect::<Vec<_>>();
     results.sort_by(|(a, _), (b, _)| a.cmp(b));
 
@@ -50,36 +58,53 @@ fn display<'a>(results: &HashMap<usize, &'a str>) {
     }
 }
 
-/// Search for the given query string in the given contents, case-sensitive.
-fn search<'a>(query: &'a str, contents: &'a str) -> HashMap<usize, &'a str> {
+/// Lazily search for the given query string in the given contents, case-sensitive.
+fn search_lazy<R: BufRead>(
+    query: &str,
+    reader: R,
+) -> Result<HashMap<usize, String>, Box<dyn std::error::Error>> {
     let mut results = HashMap::new();
 
-    for (idx, line) in contents.lines().enumerate() {
+    for (idx, line) in reader.lines().enumerate() {
+        let line = line?;
+
         if line.contains(query) {
-            results.insert(idx + 1, line);
+            results.insert(idx + 1, line.into());
         }
     }
 
-    results
+    Ok(results)
 }
 
-/// Search for the given query string in the given contents, case-insensitive.
-fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> HashMap<usize, &'a str> {
+/// Lazily search for the given query string in the given contents, case-insensitive.
+fn search_case_insensitive_lazy<R: BufRead>(
+    query: &str,
+    reader: R,
+) -> Result<HashMap<usize, String>, Box<dyn std::error::Error>> {
     let mut results = HashMap::new();
     let query = query.to_lowercase();
 
-    for (idx, line) in contents.lines().enumerate() {
+    for (idx, line) in reader.lines().enumerate() {
+        let line = line?;
+
         if line.to_lowercase().contains(&query) {
             results.insert(idx + 1, line);
         }
     }
 
-    results
+    Ok(results)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
     use super::*;
+
+    /// Create a reader from the given contents using a cursor instead of a file, for testing purposes only.
+    fn create_reader(contents: &str) -> BufReader<Cursor<&str>> {
+        BufReader::new(Cursor::new(contents))
+    }
 
     #[test]
     fn case_sensitive() {
@@ -90,10 +115,12 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
+        let reader = create_reader(contents);
+        let result = search_lazy(query, reader).unwrap();
         let mut expected = HashMap::new();
-        expected.insert(2, "safe, fast, productive.");
+        expected.insert(2, "safe, fast, productive.".into());
 
-        assert_eq!(expected, search(query, contents));
+        assert_eq!(expected, result);
     }
 
     #[test]
@@ -105,10 +132,12 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
+        let reader = create_reader(contents);
+        let result = search_case_insensitive_lazy(query, reader).unwrap();
         let mut expected = HashMap::new();
-        expected.insert(1, "Rust:");
-        expected.insert(4, "Trust me.");
+        expected.insert(1, "Rust:".into());
+        expected.insert(4, "Trust me.".into());
 
-        assert_eq!(expected, search_case_insensitive(query, contents));
+        assert_eq!(expected, result);
     }
 }
